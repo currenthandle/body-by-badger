07533876e2a1d05a932113995f9bd5bb4bf8cd98
Ubuntu <ubuntu@ip-172-31-13-244.us-west-1.compute.internal>
2016-11-15 18:44:38 +0000

    added nav.js componenet

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to Body By Badger</h2>
+          <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
